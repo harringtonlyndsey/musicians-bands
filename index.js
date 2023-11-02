8015b59e573e2b6c22ecb047ce9bb66481e3f91d
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-11-02 16:25:07 +0000

    Lyndsey

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const { Band } = require('./models/Band')
-const { Musician } = require('./models/Musician')
-const { Song } = require("./models/Song")
+const { Band } = require("./models/Band");
+const { Musician } = require("./models/Musician");
+const { Song } = require("./models/Song");
 // Define associations here
 
+Musician.hasMany(Band);
+Band.belongsTo(Musician);
 
-
+Song.belongsToMany(Band, { through: "song-band" });
+Band.belongsToMany(Song, { through: "song-band" });
 module.exports = {
-    Band,
-    Musician,
-    Song
+  Band,
+  Musician,
+  Song,
 };
